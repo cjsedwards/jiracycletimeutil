8ce1bf565ab694f9b177a4d3f3fe9ae7f54d98c8
Chris Edwards <chris.edwards@yyc-cedwards-mp.hitronhub.home>
2017-03-26 13:37:35 -0600

    Changed script to run a JQL query rather than just returning a single issue

--- queryJira.py
+++ queryJira.py
@@ -7,15 +7,17 @@ if __name__ == '__main__':
     parser.add_option('-u', '--user', dest='user', default="", help='Username to access JIRA')
     parser.add_option('-p', '--password', dest='password', default="", help='Password to access JIRA')
     parser.add_option('-j', '--jira', dest='jira_url', default="", help='JIRA Base URL, ex: https://jira.atlassian.com')
-    parser.add_option('-i', '--issue', dest='issue', default="", help='JIRA Issue, ex: DEV-2814')
+    parser.add_option('-q', '--query', dest='query', default="", help='JQL Query')
 
     (options, args) = parser.parse_args()
 
-    if(options.user == "" or options.password == "" or options.jira_url == ""):
+    if(options.user == "" or options.password == "" or options.jira_url == "" or options.query == ""):
         parser.print_help()
         exit()
 
-    r = requests.get( options.jira_url + '/rest/api/latest/issue/' + options.issue + "?expand=changelog", auth=(options.user, options.password) )
+    payload = {"jql":options.query, "expand":"changelog"}
+
+    r = requests.get( options.jira_url + '/rest/api/2/search', params=payload, auth=(options.user, options.password) )
 
     parsed = json.loads(r.text)
 
