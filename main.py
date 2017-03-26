376f7a726a03b4eb0eb81901cf7439f1d8c303f3
Chris Edwards <chris.edwards@yyc-cedwards-mp.hitronhub.home>
2017-03-26 07:59:47 -0600

    Initial commit of very simple script to query from Jira

---
+++ main.py
@@ -0,0 +1,19 @@
+import requests
+import optparse
+
+if __name__ == '__main__':
+    parser = optparse.OptionParser()
+    parser.add_option('-u', '--user', dest='user', default="", help='Username to access JIRA')
+    parser.add_option('-p', '--password', dest='password', default="", help='Password to access JIRA')
+    parser.add_option('-j', '--jira', dest='jira_url', default="", help='JIRA Base URL, ex: https://jira.atlassian.com')
+    parser.add_option('-i', '--issue', dest='issue', default="", help='JIRA Issue, ex: DEV-2814')
+
+    (options, args) = parser.parse_args()
+
+    if(options.user == "" or options.password == "" or options.jira_url == ""):
+        parser.print_help()
+        exit()
+
+    r = requests.get( options.jira_url + '/rest/api/latest/issue/' + options.issue, auth=(options.user, options.password) )
+
+    print(r.text)
